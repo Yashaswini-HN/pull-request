cd02867dbc212cff13f529c63750bfcaeb69c10e
Yashaswini HN <87577757+Yashaswini-HN@users.noreply.github.com>
2021-08-29 22:32:35 +0530

    Create Adding 2 num.c

---
+++ Adding 2 num.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+void main
+{
+ int num1,num2,sum;
+ printf("Enter the value of num1\n");
+ scanf("%d",&num1);
+ printf("Enter the value of num2\n");
+ scanf("%d",&num2);
+ sum=num1+num2;
+ printf("sum of two numbers =%d",sum);
+ }
