0c52757812628fc7cb9824d56a2794de9db6108a
chandanakumar2 <77927637+chandanakumar2@users.noreply.github.com>
2021-08-31 12:03:19 +0530

    Update sum-of-two-numbers.c

--- sum-of-two-numbers.c
+++ sum-of-two-numbers.c
@@ -1,11 +1,10 @@
 #include<stdio.h>
 void main()
 {
- int num1,num2,sum;
+ int num1,num2;
  printf("Enter the value of num1\n");
  scanf("%d",&num1);
  printf("Enter the value of num2\n");
  scanf("%d",&num2);
- sum=num1+num2;
- printf("sum of two numbers =%d",sum);
+ printf("sum of two numbers =%d",num1+num2);
  }
